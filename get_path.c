78cc5ccca177d3b4fdfa8809c219a54e2671ff7f
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-08-22 20:31:01 +0300

    get the path from the env and strtok it

--- get_path.c
+++ get_path.c
@@ -1,5 +1,8 @@
-#include "main.h"
+#include "shell.h"
 /**
+ * _path - .....
+ * @env:the env.
+ * Return:the path.
  */
 char *_path(char **env)
 {
@@ -8,34 +11,37 @@ char *_path(char **env)
 
 	while (env[path_idx] != NULL)
 	{
-		if (strncmp(env[path_idx], "PATH", 4) == 0)
+		if (_strncmp(env[path_idx], "PATH", 4) == 0)
 			break;
 		path_idx++;
 	}
 
-	path_len = strlen(env[path_idx]);
+	path_len = _strlen(env[path_idx]);
 
 	path = malloc(path_len - 4);
 	if (!path)
-		return NULL;
+		return (NULL);
 
-	strcpy(path, env[path_idx] + 5);
+	_strcpy(path, env[path_idx] + 5);
 	return (path);
-	
 }
-
+/**
+ * _array_path - ......
+ * @path:the path.
+ * Return:array of path.
+ */
 char **_array_path(char *path)
 {
 	int i = 0, no_del = 0;
-	char **array_path, *token, *tmp;
+	char **array_path, *token;
+
 	while (path[i])
 	{
 		if (path[i] == ':')
 			no_del++;
 		i++;
 	}
-	
-	array_path = malloc(sizeof(char *) * (no_del + 2)); //==> 2
+	array_path = malloc(sizeof(char *) * (no_del + 2));
 
 	i = 0;
 	token = strtok(path, ":");
@@ -47,10 +53,17 @@ char **_array_path(char *path)
 	array_path[i] = NULL;
 	return (array_path);
 }
+/**
+ * concat_command - .....
+ * @array_path: array of path.
+ * @command: the command.
+ * Return:the concated array.
+ */
 char **concat_command(char **array_path, char *command)
 {
 	int i = 0, total_len = 0;
 	char **concated_array, *temp;
+
 	while (array_path[i])
 		i++;
 
@@ -61,24 +74,29 @@ char **concat_command(char **array_path, char *command)
 	{
 		if (i == 0)
 		{
-			concated_array[0] = malloc(strlen(command) + 1);
-			strcpy(concated_array[0], command);
+			concated_array[0] = malloc(_strlen(command) + 1);
+			_strcpy(concated_array[0], command);
 		}
-		total_len = strlen(command) + strlen(array_path[i]) + 2; /* /bin/ls */
+		total_len = _strlen(command) + _strlen(array_path[i]) + 2;
 		concated_array[i + 1] = malloc(total_len);
 		temp = malloc(total_len);
-		//strcpy(temp, "");
-		strcpy(temp, array_path[i]);
-		strcpy(concated_array[i + 1], strcat(strcat(temp, "/"), command));
+		_strcpy(temp, array_path[i]);
+		_strcpy(concated_array[i + 1], _strcat(strcat(temp, "/"), command));
 		i++;
 		free(temp);
 	}
 	concated_array[i + 1] = NULL;
 	return (concated_array);
 }
+/**
+ * free_2darr - ....
+ * @array_path:the array of path.
+ * Reutrn:void.
+ */
 void free_2darr(char **array_path)
 {
 	int i = 0;
+
 	while (array_path[i])
 		free(array_path[i++]);
 	free(array_path[i]);
