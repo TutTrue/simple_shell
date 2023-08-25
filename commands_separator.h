0931ad157675850ec98635aec6b7b2d9ff9f73fe
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-08-25 13:56:09 +0300

    handle ; between commands

---
+++ commands_separator.h
@@ -0,0 +1,33 @@
+#ifndef COMMANDS_SEPARATOR_H
+#define COMMANDS_SEPARATOR_H
+/**
+ * struct nodecmd_s - node that contains cmd
+ * @cmd: command
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
+typedef struct nodecmd_s
+{
+	char *cmd;
+	struct nodecmd_s *next;
+} node_cmd;
+
+/**
+ * struct listcmd_s - singly linked list
+ * @head: points to the head node
+ * @cur: points to the cur node
+ * @tail: points to the tail node
+ * Description: singly linked list node structure
+ */
+typedef struct listcmd_s
+{
+	node_cmd *head;
+	node_cmd *cur;
+	node_cmd *tail;
+} list_cmd;
+void add_node(list_cmd *my_list, char *cmd);
+void print_list(list_cmd *my_list);
+void free_list(list_cmd *my_list);
+list_cmd *commands_separator(char *line);
+
+#endif // COMMANDS_SEPARATOR_H
--- shell.c
+++ shell.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+#include "commands_separator.h"
 
 int main(int ar, char **av, char **env)
 {
@@ -12,6 +13,7 @@ void mainloop(char **av, char**env)
 	char *line = NULL;
 	size_t buf_size = 0;
 	int line_number = 0;
+	list_cmd *sep_cmds;
 	(void) av;
 
 
@@ -23,18 +25,26 @@ void mainloop(char **av, char**env)
 		if (_getline(&line, &buf_size, 0) == -1)
 		{
 			if (isatty(0))
-			printf("\n");
+				printf("\n");
 			break;
 		}
 
-		if (just_spaces(line))
+		sep_cmds = commands_separator(line);
+
+		while (sep_cmds->cur)
 		{
-			free(line);
-			continue;
-		}	
-		strtrim(line);
-		line_number++;
-		execute_program(line, env, line_number);
+			if (just_spaces(sep_cmds->cur->cmd))
+			{
+				//free(line);
+				sep_cmds->cur = sep_cmds->cur->next;
+				continue;
+			}
+			strtrim(sep_cmds->cur->cmd);
+			line_number++;
+			execute_program(sep_cmds->cur->cmd, env, line_number);
+			sep_cmds->cur = sep_cmds->cur->next;
+		}
+		free_list(sep_cmds);
 		free(line);
 	}
 }
